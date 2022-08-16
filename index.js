fa5511d0d1cfbf323504597bcec7ef121e46bab6
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-16 07:21:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser ='Bucky Roberts';
+let welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK,   ${currentUser.toUpperCase()}!`;
+// const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() +'!';
+const shortGreeting =`Welcome, ${currentUser[0]}!`
+
